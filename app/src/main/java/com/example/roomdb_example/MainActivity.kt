7f0d7b612a78cb46dc767e76cc0082ee2c731434
zqudwnsz@naver.com <zqudwnsz@naver.com>
2022-11-21 23:14:21 +0900

    Add: Room DB를 사용하는 Activity

--- app/src/main/java/com/example/roomdb_example/MainActivity.kt
+++ app/src/main/java/com/example/roomdb_example/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.roomdb_example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myText : TextView = findViewById(R.id.textView)
+
+        var newUser = User("홍길동", "20", "010-1234-1234")
+
+//        //싱글톤 패턴을 사용하지 않은경우
+//        val db = Room.databaseBuilder(
+//            applicationContext,
+//            UserDatabase::class.java,
+//            "user-database"
+//        ).build()
+//        db.userDao().insert(newUser)
+
+        //싱글톤 패턴을 사용한 경우
+        val db = UserDatabase.getInstance(applicationContext)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            db!!.userDao().insert(newUser)
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+//            db!!.userDao().deleteUserByName("홍길동")
+            myText.text = db!!.userDao().getAll().toString()
+        }
+
+
+
+
+
+
+
+
     }
 }
